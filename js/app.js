1ee98a91e6ec2aa108fda37e0d0ca07116155183
mikel-ac <mikel.agirrezabalaga@gmail.com>
2026-05-05 12:20:59 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
  * Mi Gastro — app.js v5b
  */
 
-window.MAPS_API_KEY = 'TU_API_KEY_AQUI';
+window.MAPS_API_KEY = 'AIzaSyDW6pyW3oid6Fvz87JBSVSdYnH_sDd-rw8';
 
 const State = {
   allRests: [], ciudades: {}, currentCiudad: null,
