1be0ce43664b0d528767a247a776f2cb22787224
mikel-ac <mikel.agirrezabalaga@gmail.com>
2026-05-06 10:16:18 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const USE_CACHE = false;
-const VERSION = 'v14';
+const VERSION = 'v15';
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(() => self.clients.claim()));
