f52ca80cd89a78e0c790e6e6c2d14df039b68c73
mikel-ac <mikel.agirrezabalaga@gmail.com>
2026-05-06 18:32:21 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const USE_CACHE = false;
-const VERSION = 'v18';
+const VERSION = 'v19';
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(() => self.clients.claim()));
